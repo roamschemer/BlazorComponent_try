8b017ad65658994b2a1bfe41561709645db32b54
roamschemer <roamschemer@gmail.com>
2021-08-13 00:42:45 +0900

    コンポーネントとのやりとり理解

--- BlazorComponent_try/Program.cs
+++ BlazorComponent_try/Program.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using BlazorStrap;
 
 namespace BlazorComponent_try {
     public class Program {
@@ -15,6 +16,7 @@ namespace BlazorComponent_try {
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddBootstrapCss();
 
             await builder.Build().RunAsync();
         }
